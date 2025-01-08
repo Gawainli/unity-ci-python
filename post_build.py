e9ab3bf2461fc7ea0328edc29330c16b83deec6c
Gawain <gawainli@outlook.com>
2025-01-08 17:38:45 +0800

    feat:new bak path

--- post_build.py
+++ post_build.py
@@ -5,34 +5,34 @@ import os
 import cpy_tool
 
 
-def _bak_old_bundles():
+def _bak_old_bundles(build_target:str):
     """_summary_"""
     logger.info("Bak old bundles.")
     bak_time = datetime.now().strftime("%y-%m-%d-%H%M%S")
 
-    bak_path = Path(os.environ["BUNDLE_COPY_TO"]).parent / "bak" / f"bak_{bak_time}"
+    bak_path = Path(os.environ["BUNDLE_COPY_TO"]).parent / "bak" / f"{build_target.lower()}"/ f"{bak_time}"
     bak_path.mkdir(parents=True, exist_ok=True)
 
     src_path = Path(os.environ["BUNDLE_COPY_TO"])
     if src_path.exists():
         cpy_tool.move_directory_tree_with_info(
-            src_path / "cdn/android", bak_path / "android"
+            src_path / "cdn"/build_target.lower(), bak_path / build_target.lower()
         )
-        cpy_tool.move_directory_tree_with_info(src_path / "cdn/ios", bak_path / "ios")
     else:
         logger.warning(f"Path {src_path} does not exist.")
 
 
 def post_build_bundles():
-    _bak_old_bundles()
     package_names = os.environ["PACKAGE_NAMES"].split(",")
     version = os.environ["BUNDLE_VERSION"]
     build_target = os.environ["BUILD_TARGET"]
+    _bak_old_bundles(build_target)
     _move_bundles(package_names, version, build_target)
 
 def post_build_app():
     build_target = os.environ["BUILD_TARGET"].lower()
     if build_target == "android":
+        logger.info("Post build for Android.")
         _move_android_apk()
     elif build_target == "ios":
         logger.warning("iOS post build is not supported yet.")
@@ -63,12 +63,12 @@ def _move_android_apk():
     to_path = Path(os.environ["BUNDLE_COPY_TO"])/"apk"
     if not to_path.exists():
         to_path.mkdir(parents=True)
+    logger.info(f"Moving APK from {from_path} to {to_path}")
     cpy_tool.copy_file_to(from_path, to_path)
-
 
 if __name__ == "__main__":
     import build_env_tool
 
     build_env_tool.set_ci_env("cfg/ci_env.ini")
-    _bak_old_bundles()
+    # _bak_old_bundles()
     # _move_bundles(["DefaultPackage", "RawPackage"], "1.0.0-25-01-06-224641", "Android")
